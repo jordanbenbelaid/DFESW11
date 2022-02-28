7d491ca7736ca544eef917c1851bdffcc3893f3a
Jordan <jordanbenbelaid@hotmail.com>
2022-02-28 10:53:52 +0000

    JDBC config added

---
+++ Jdbc/src/main/java/com/qa/config/DatabaseConfig.java
@@ -0,0 +1,11 @@
+package com.qa.config;
+
+public class DatabaseConfig {
+
+	public static String username = "root";
+	
+	public static String password = "pass";
+	
+	public static String url = "jdbc:mysql://localhost:3306/movies";
+	
+}
