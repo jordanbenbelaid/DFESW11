194db2652d203c9fb4317fa92d4def265b6b6cd8
Jordan <jordanbenbelaid@hotmail.com>
2022-02-28 12:02:30 +0000

    Added to domain class Movie

--- Jdbc/src/main/java/com/qa/domain/Movie.java
+++ Jdbc/src/main/java/com/qa/domain/Movie.java
@@ -2,4 +2,44 @@ package com.qa.domain;
 
 public class Movie {
 
+	//Attributes
+	private String name;
+	private String genre;
+	private int rating;
+	
+	//default constructor
+	public Movie() {}
+	
+	//All arguments constructor
+	public Movie(String name, String genre, int rating) {
+		this.name = name;
+		this.genre = genre;
+		this.rating = rating;
+	}
+
+	//getters and setters
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getGenre() {
+		return genre;
+	}
+
+	public void setGenre(String genre) {
+		this.genre = genre;
+	}
+
+	public int getRating() {
+		return rating;
+	}
+
+	public void setRating(int rating) {
+		this.rating = rating;
+	}
+	
 }
